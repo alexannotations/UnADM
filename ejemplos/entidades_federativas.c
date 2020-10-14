292afc0003acd0cd5c9108fc68dff4ce18b95d29
AlexAC <alexayca@yahoo.com.mx>
2020-10-13 23:12:01 -0500

    Empezando a editar en local new file:   ejemplos/entidades_federativas.c

---
+++ ejemplos/entidades_federativas.c
@@ -0,0 +1,16 @@
+/* **********************
+ * Programa para mostrar el uso de
+ * estructuras y funciones
+ * con los estados de la Republica Mexicana
+ * 
+ * Alejandro Ayala
+ * Octubre 2020
+ * 
+ * **********************/
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
+
