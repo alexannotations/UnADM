c2e64c5a0a4f43d6199569ccf088b5237fa470ea
alexayca <90019860+alexayca@users.noreply.github.com>
2022-01-02 20:02:21 -0600

    actividad 1 y 2 de U4

--- oop_java/1_actividades/productos/unidimensional.java
+++ oop_java/1_actividades/productos/unidimensional.java
@@ -1,7 +1,7 @@
 /* * * *
  * U4 A1
  * Crea un proyecto, que te permita leer y almacenar el nombre y precio de 10 productos comerciales; 
- * asÃ­ mismo el programa debe mostrar su nombre, su precio y posiciÃ³n para precios mayores o iguales a $500. 
+ * así mismo el programa debe mostrar su nombre, su precio y posición para precios mayores o iguales a $500. 
  * @author AAC
  * 
  * */
@@ -9,7 +9,7 @@
 package productos;
 import java.util.Scanner;
 
-public class Inicio {
+public class unidimensional {
     
 	public static void main(String args[]) {	
         int index = 10; //Se almacena en una variable las posiciones del arreglo
@@ -18,21 +18,27 @@ public class Inicio {
         String producto[] = new String[index];
         float precio[] = new float[index];
 
-        System.out.println("\n\tPrograma que muestra solo los productos con precio mayor a $500.00\n");
+        System.out.println("\n\tProductos\n");
 
         System.out.println("\tIntroduce los datos segun se solicite:");
         for (int i = 0; i < index; i++) {
             System.out.print("\t\tNombre del producto [" +(i+1) +"]: ");
             producto[i] = keyBoard.nextLine();
             System.out.print("\t\tPrecio del producto [" +(i+1) +"]: ");
-            precio[i] = keyBoard.nextFloat();
-            keyBoard.nextLine(); /*(en el buffer queda el retorno de carro)
+            try {
+                precio[i] = keyBoard.nextFloat();
+
+            }catch(Exception e) {
+            	System.out.println(e);
+            }
+            keyBoard.nextLine(); /* (en el buffer queda el retorno de carro)
             captura el intro (nextLine) que no guardo nextFloat al almacenar el numero, 
-            sin asignarlo, esto ocurre al querer leer un String despues de leer un numero*/
+            sin asignarlo, esto ocurre al querer leer un String despues de leer un numero */
         }
 
-        //ImpresiÃ³n en pantalla
-        System.out.printf("\n\t%s\t%12.8s\t -> %.8s\n", "ID", "Producto", "Precio");
+        //Impresión en pantalla
+        System.out.println("\n\tPrograma que muestra solo los productos con precio mayor a $500.00\n");
+        System.out.printf("\n\t%s\t%12.8s\t -> %.8s\n", "ID", "Producto", "Precio");	// función de impresión con formato
         System.out.println("\t----------------------------------------");
 
         //Ciclo para mostrar unicamente precios mayores a 500
